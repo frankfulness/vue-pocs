8a27ca049e204139d2d4a0f0bfb7dc48a39307b2
Frankfulness <Fullstackfrank@gmail.com>
2025-01-17 15:28:08 -0800

    First pass: Quasar's Vite Plugin Flavor

--- quasar-plugin/src/views/AboutView.vue
+++ quasar-plugin/src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <q-card class="my-card">
+      <q-card-section> Hello, I am a Quasar Card. </q-card-section>
+    </q-card>
   </div>
 </template>
 
